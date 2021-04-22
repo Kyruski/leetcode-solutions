e169afd5152810ffceb79c92694f10bdd26ce612
kyruski <jamescboyett@gmail.com>
2021-04-22 08:16:11 -0700

    Completes 1603 in javascript

--- javascript/1603.design-parking-system.js
+++ javascript/1603.design-parking-system.js
@@ -4,7 +4,9 @@
  * @param {number} small
  */
 var ParkingSystem = function(big, medium, small) {
-    
+  this.big = big;
+  this.medium = medium;
+  this.small = small;
 };
 
 /** 
@@ -12,7 +14,23 @@ var ParkingSystem = function(big, medium, small) {
  * @return {boolean}
  */
 ParkingSystem.prototype.addCar = function(carType) {
-    
+  switch (carType) {
+    case 1:
+      if (this.big > 0) {
+        this.big--;
+        return true;
+      } else return false;
+    case 2:
+      if (this.medium > 0) {
+        this.medium--;
+        return true;
+      } else return false;
+    case 3:
+      if (this.small > 0) {
+        this.small--;
+        return true;
+      } else return false;
+  }
 };
 
 /** 
