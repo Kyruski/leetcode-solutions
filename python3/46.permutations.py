e32920737060a1dcba00a93cb974e36b14311859
kyruski <jamescboyett@gmail.com>
2021-04-24 08:21:41 -0700

    Completes 46 for python3

--- python3/46.permutations.py
+++ python3/46.permutations.py
@@ -1,2 +1,24 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        indexList = []
+        output = []
+        currPermutation = []
+
+        def runPermutations():
+            for i in range(len(nums)):
+                if i in indexList:
+                    continue
+                else:
+                    indexList.append(i)
+                    currPermutation.append(nums[i])
+
+                if len(currPermutation) == len(nums):
+                    output.append(currPermutation.copy())
+                else:
+                    runPermutations()
+
+                currPermutation.pop()
+                indexList.pop()
+
+        runPermutations()
+        return output
