df4993209a958397fe000b1f83c4d825e3b8072f
kyruski <jamescboyett@gmail.com>
2021-04-25 08:10:25 -0700

    Completes 78 for python3

--- python3/78.subsets.py
+++ python3/78.subsets.py
@@ -1,2 +1,14 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        output = [[]]
+        currArray = []
+
+        def recurse(currNumsList: List[int]):
+            for index, num in enumerate(currNumsList):
+                currArray.append(num)
+                output.append(currArray.copy())
+                recurse(currNumsList[index + 1:])
+                currArray.pop()
+
+        recurse(nums)
+        return output
