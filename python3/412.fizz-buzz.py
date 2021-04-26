16df16c8ed429f2fc3104466086df87443c1ff39
kyruski <jamescboyett@gmail.com>
2021-04-26 09:51:50 -0700

    Completes 412 for python3

--- python3/412.fizz-buzz.py
+++ python3/412.fizz-buzz.py
@@ -1,2 +1,18 @@
 class Solution:
+    def __init__(self):
+        self.output = []
+
     def fizzBuzz(self, n: int) -> List[str]:
+        string = ''
+        for i in range(n):
+            num = i + 1
+            if num % 3 == 0:
+                string += 'Fizz'
+            if num % 5 == 0:
+                string += 'Buzz'
+            if string == '':
+                string += str(num)
+            self.output.append(string)
+            string = ''
+
+        return self.output
