7536914d5e9ea1d88d3f4a6b99d72fb1e2811bc7
kyruski <jamescboyett@gmail.com>
2021-04-27 08:03:02 -0700

    Completes 230 for javascript

--- javascript/230.kth-smallest-element-in-bst.js
+++ javascript/230.kth-smallest-element-in-bst.js
@@ -12,5 +12,18 @@
  * @return {number}
  */
 var kthSmallest = function(root, k) {
+  const stack = []
+  let curr = root;
+  const values = [];
+  while (curr || stack.length) {
+    if (curr) {
+      stack.push(curr.right);
+      values.push(curr.val);
+      curr = curr.left
+    } else {
+      curr = stack.pop()
+    }
+  }
 
+  return values.sort((a, b) => a - b)[k - 1];
 };
