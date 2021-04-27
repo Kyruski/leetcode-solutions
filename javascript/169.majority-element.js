5502461f9269b5a7f2000fbbdacc69df691d0bc8
kyruski <jamescboyett@gmail.com>
2021-04-27 08:12:42 -0700

    Completes 169 for javascript

--- javascript/169.majority-element.js
+++ javascript/169.majority-element.js
@@ -3,5 +3,18 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    
+  const dict = {}
+
+  for (let item of nums) {
+    dict[item] = dict[item] ? dict[item] + 1 : 1;
+  }
+  let max = -Infinity;
+  let num = -Infinity;
+  for (let key in dict) {
+    if (dict[key] > max) {
+      max = dict[key];
+      num = key;
+    }
+  }
+  return num;
 };
