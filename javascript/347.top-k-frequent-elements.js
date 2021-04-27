2dd72f43ff82a5579fd5db642907edc46d3784ee
kyruski <jamescboyett@gmail.com>
2021-04-27 07:50:26 -0700

    Completes 347 for javascript

--- javascript/347.top-k-frequent-elements.js
+++ javascript/347.top-k-frequent-elements.js
@@ -4,5 +4,13 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-  
+  const dict = {};
+  for (let item of nums) {
+    dict[item] = dict[item] ? dict[item] + 1 : 1;
+  }
+  const array = [];
+  for (let key in dict) {
+    array.push([key, dict[key]]);
+  }
+  return array.sort((a, b) => b[1] - a[1]).slice(0, k).map(item => item[0]);
 };
