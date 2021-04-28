1b700e127d14aad5e6bcc7e88f6ba10f6e20cd3d
kyruski <jamescboyett@gmail.com>
2021-04-28 08:17:15 -0700

    Completes 202 for javascript

--- javascript/202.happy-number.js
+++ javascript/202.happy-number.js
@@ -3,5 +3,16 @@
  * @return {boolean}
  */
 var isHappy = function(n) {
-  
+  const prev = {}
+  while (!prev[n] && n !== 1) {
+    prev[n] = true;
+    let numArray = String(n).split('');
+    let curr = 0;
+    for (let item of numArray) {
+      curr += Number(item)**2
+    }
+    n = curr;
+  }
+
+  return n === 1;
 };
