6acf27c92a3fe0b6d14c8d3662e52b76b6813bbc
kyruski <jamescboyett@gmail.com>
2021-04-29 07:29:27 -0700

    Completes 378 for javascript

--- javascript/378.kth-smallest-element-in-a-sorted-matrix.js
+++ javascript/378.kth-smallest-element-in-a-sorted-matrix.js
@@ -4,5 +4,11 @@
  * @return {number}
  */
 var kthSmallest = function(matrix, k) {
-
+  const output = []
+  for (let item of matrix) {
+    for (let item2 of item) {
+      output.push(item2);
+    } 
+  }
+  return output.sort((a, b) => a - b)[k - 1];
 };
