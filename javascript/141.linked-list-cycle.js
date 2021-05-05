76ea85542d5c3653d9a9b0da8f6f39bb7a6cc0f8
kyruski <jamescboyett@gmail.com>
2021-05-05 08:03:44 -0700

    Completes 141 for javascript

--- javascript/141.linked-list-cycle.js
+++ javascript/141.linked-list-cycle.js
@@ -11,5 +11,16 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-  
+  if (!head) return false;
+  let p1 = head;
+  let p2;
+  if (head.next) p2 = head.next;
+  else return false;
+  while (p1 && p2) {
+    if (p1 === p2) return true;
+    p1 = p1.next;
+    if (p2.next !== null) p2 = p2.next.next;
+    else return false;
+  }
+  return false;
 };
