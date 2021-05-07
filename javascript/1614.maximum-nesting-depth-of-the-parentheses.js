bde24174e804475b62a08250d9bf5a54b1485d2b
kyruski <jamescboyett@gmail.com>
2021-05-07 07:31:11 -0700

    Completes 1614 for javascript

--- javascript/1614.maximum-nesting-depth-of-the-parentheses.js
+++ javascript/1614.maximum-nesting-depth-of-the-parentheses.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var maxDepth = function(s) {
-  
+  let max = 0;
+  let stackSize = 0;;
+
+  for (let item of s) {
+    if (item === '(') {
+      stackSize++;
+      if (stackSize > max) max = stackSize;
+    } else if (item === ')') stackSize--;
+  }
+
+  return max;
 };
