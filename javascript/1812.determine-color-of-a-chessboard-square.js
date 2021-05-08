a808ccb4e23cfdb6bb1f50daa9f7c2cb1f565021
kyruski <jamescboyett@gmail.com>
2021-05-08 10:06:48 -0700

    Completes 1812 for javascript

--- javascript/1812.determine-color-of-a-chessboard-square.js
+++ javascript/1812.determine-color-of-a-chessboard-square.js
@@ -3,4 +3,9 @@
  * @return {boolean}
  */
 var squareIsWhite = function(coordinates) {
+  return (coordinates.charCodeAt(0) % 2 === 1) ? Number(coordinates[1]) % 2 === 0 : Number(coordinates[1]) % 2 === 1;
+};
+
+var squareIsWhite = function(coordinates) {
+  return (coordinates.charCodeAt(0) % 2 === 0) === return (coordinates.charCodeAt(0) % 2 === 0) === (Number(coordinates[1]) % 2 === 1);;
 };
