f2b3f4cf82c3de0e74b148bec38bcf2fb5849ff5
kyruski <jamescboyett@gmail.com>
2021-05-09 08:43:46 -0700

    Completes 1768 for javascript

--- javascript/1768.merge-strings-alternately.js
+++ javascript/1768.merge-strings-alternately.js
@@ -1,8 +1,12 @@
-/**
- * @param {string} word1
- * @param {string} word2
- * @return {string}
- */
-var mergeAlternately = function(word1, word2) {
-
-};
+let output = '';
+for (let i = 0; i <= word1.length; i++) {
+  if (i === word2.length) {
+    output += word1.slice(i);
+    return output
+  } else if (i === word1.length) {
+    output += word2.slice(i);
+    return output
+  }
+  output += word1[i] + word2[i]
+}
+return output;
