2c1cc1fc6cefc72b6b3a30fe52a4faba46286e2f
kyruski <jamescboyett@gmail.com>
2021-05-12 10:21:21 -0700

    Completes 1051 for javascript

--- javascript/1051-height-checker.js
+++ javascript/1051-height-checker.js
@@ -3,5 +3,6 @@
  * @return {number}
  */
 var heightChecker = function(heights) {
-  
+  const sorted = [...heights].sort((a, b) => a - b);
+  return heights.reduce((t, c, i) =>  t += (c === sorted[i]) ? 0 : 1, 0)
 };
