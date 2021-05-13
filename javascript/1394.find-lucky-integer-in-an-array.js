328cc921ba9aec763de9c81156161913322b6098
kyruski <jamescboyett@gmail.com>
2021-05-13 08:46:02 -0700

    Completes 1394 for javascript

--- javascript/1394.find-lucky-integer-in-an-array.js
+++ javascript/1394.find-lucky-integer-in-an-array.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var findLucky = function(arr) {
-  
+  const dict = {};
+  let output = -1;
+  for (let item of arr) {
+    dict[item] = (dict[item]) ? dict[item] + 1 : 1;
+  }
+  for (let key in dict) {
+    if (dict[key] === parseInt(key)) {
+      output = (output < parseInt(key)) ? key : output;
+    }
+  }
+  return output;
 };
