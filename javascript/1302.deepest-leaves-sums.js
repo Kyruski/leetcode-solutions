0ef51a91556ea8c13ded62ddf6a23d7d8a5c48e4
kyruski <jamescboyett@gmail.com>
2021-05-14 09:38:50 -0700

    Completes 1302 for javascript

--- javascript/1302.deepest-leaves-sums.js
+++ javascript/1302.deepest-leaves-sums.js
@@ -10,6 +10,20 @@
  * @param {TreeNode} root
  * @return {number}
  */
- var deepestLeavesSum = function(root) {
-    
+var deepestLeavesSum = function(root) {
+  let maxDepth = 0;
+  let sum = 0;
+  const traverse = (node, depth = 0) => {
+    if (!node) return;
+    if (depth > maxDepth) {
+      sum = node.val;
+      maxDepth = depth;
+    } else if (depth === maxDepth) {
+      sum += node.val
+    }
+    traverse(node.left, depth + 1);
+    traverse(node.right, depth + 1)
+  }
+  traverse(root);
+  return sum;
 };
