2c8687019302cbb8f3c9fc02e829247cb8b193fb
kyruski <jamescboyett@gmail.com>
2021-05-16 13:53:18 -0700

    Completes 1379 for javascript

--- javascript/1379.find-a-corresponding-node-of-a-binary-tree-in-a-clone.js
+++ javascript/1379.find-a-corresponding-node-of-a-binary-tree-in-a-clone.js
@@ -13,5 +13,19 @@
  */
 
 var getTargetCopy = function(original, cloned, target) {
-  
+  let output = null;
+
+  const traverse = (node1, node2) => {
+    if (output || !node1) return;
+    if (node1 === target) {
+      output = node2;
+      return;
+    }
+    traverse(node1.left, node2.left);
+    traverse(node1.right, node2.right);
+  }
+
+  traverse(original, cloned);
+
+  return output;
 };
