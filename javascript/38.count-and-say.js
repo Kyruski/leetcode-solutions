099253f9cdde9ff6b8411ad0297185fd497d3d01
kyruski <jamescboyett@gmail.com>
2021-05-19 11:33:56 -0700

    Completes 38 for javascript

--- javascript/38.count-and-say.js
+++ javascript/38.count-and-say.js
@@ -3,5 +3,31 @@
  * @return {string}
  */
 var countAndSay = function(n) {
-  
+  let output;
+  let currStr = '1';
+  for (let i = 0; i < n; i++) {
+    if (i !== 0) {
+      let newStr = '';
+      let count = 0;
+      let currNum;
+      for (let item of currStr) {
+        if (currNum) {
+          if (item === currNum) {
+            count++
+          } else {
+            newStr += count + currNum;
+            count = 1;
+            currNum = item;
+          }
+        } else {
+          currNum = item;
+          count++;
+        }
+      }
+      newStr += count + currNum;
+      currStr = newStr;
+    }
+    output = currStr;
+  }
+  return output;
 };
