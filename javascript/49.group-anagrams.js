77b4ce4c04916638ed1c411419fa9c4cf9416a62
kyruski <jamescboyett@gmail.com>
2021-05-20 11:24:56 -0700

    Completes 49 for javascript

--- javascript/49.group-anagrams.js
+++ javascript/49.group-anagrams.js
@@ -3,5 +3,14 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-  
+  let output = {};
+  for (let i = 0; i < strs.length; i++) {
+    let item = strs[i].split('').sort().join('')
+    if (output[item] ) {
+      output[item].push(strs[i])
+    } else {
+      output[item] = [strs[i]]
+    }
+  }
+  return Object.values(output);
 };
