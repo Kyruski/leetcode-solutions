faa56bfe201d25c2300c2482c2362becf8ec32e4
kyruski <jamescboyett@gmail.com>
2021-05-21 10:45:58 -0700

    Completes 287 for javascript

--- javascript/287.find-the-duplicate-number.js
+++ javascript/287.find-the-duplicate-number.js
@@ -3,5 +3,10 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-  
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) return nums[i];
+    }
+  }
+  return null;
 };
