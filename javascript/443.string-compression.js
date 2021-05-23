ab16665991f5337f464f85b55168321fd560e9c0
kyruski <jamescboyett@gmail.com>
2021-05-22 21:48:37 -0700

    Completes 443 for javascript

--- javascript/443.string-compression.js
+++ javascript/443.string-compression.js
@@ -3,4 +3,26 @@
  * @return {number}
  */
  var compress = function(chars) {
+  let s = '';
+  let count = 1;
+  let curr;
+  let currIndex = 0;
+  let i;
+  for (i = 0; i <= chars.length; i++) {
+    if (!curr) {
+      curr = chars[i];
+    } else if (chars[i] === curr) {
+      count++
+    } else {
+      s += (count === 1) ? curr : curr + count;
+      s = s.split('');
+      chars.splice(currIndex, i - currIndex, ...s);
+      i = currIndex + s.length;
+      currIndex += s.length;
+      s = '';
+      curr = chars[i];
+      count = 1;
+    }
+  }
+  return chars.length;
 };
