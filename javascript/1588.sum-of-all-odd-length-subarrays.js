a868e8eb339e06fc204137041b17d9b48b20e717
kyruski <jamescboyett@gmail.com>
2021-05-24 09:24:51 -0700

    Completes 1588 for javascript

--- javascript/1588.sum-of-all-odd-length-subarrays.js
+++ javascript/1588.sum-of-all-odd-length-subarrays.js
@@ -3,5 +3,17 @@
  * @return {number}
  */
 var sumOddLengthSubarrays = function(arr) {
-  
+  let total = 0;
+  let currArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    currArr = [arr[i]];
+    let j = i + 1;
+    total += currArr.reduce((total, curr) => total += curr);
+    while(j < arr.length) {
+      currArr.push(arr[j])
+      if (currArr.length % 2 === 1) total += currArr.reduce((total, curr) => total += curr);
+      j++
+    }
+  }
+  return total;
 };
