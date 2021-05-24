2517f72be68c5bdcafc637aa2f6a23142fe42eb3
kyruski <jamescboyett@gmail.com>
2021-05-23 17:55:54 -0700

    Completes 1725 for javascript

--- javascript/1725.number-of-rectangles-that-can-form-the-largest-square.js
+++ javascript/1725.number-of-rectangles-that-can-form-the-largest-square.js
@@ -3,5 +3,14 @@
  * @return {number}
  */
 var countGoodRectangles = function(rectangles) {
-  
+  let largest = -Infinity;
+  return rectangles.reduce((total, curr) => {
+    let lower = curr[0] < curr[1] ? curr[0] : curr[1];
+    if (lower > largest) {
+      largest = lower;
+      return 1;
+    } else if (lower === largest) {
+      return total + 1;
+    } else return total;
+  }, 0)
 };
