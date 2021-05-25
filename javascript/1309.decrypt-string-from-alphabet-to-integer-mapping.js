dc34068a358ee3643c6cef6bc9975b2d64f21f76
kyruski <jamescboyett@gmail.com>
2021-05-25 09:33:20 -0700

    Completes 1309 for javascript

--- javascript/1309.decrypt-string-from-alphabet-to-integer-mapping.js
+++ javascript/1309.decrypt-string-from-alphabet-to-integer-mapping.js
@@ -3,4 +3,15 @@
  * @return {string}
  */
 var freqAlphabets = function(s) {
+  const start = 96;
+  let output = '';
+  for (let i = 0; i < s.length; i++) {
+    if (s[i + 2] === '#') {
+      output += String.fromCharCode(start + parseInt(s.slice(i, i + 2)))
+      i += 2;
+    } else {
+      output += String.fromCharCode(start + parseInt(s[i]));
+    }
+  }
+  return output;
 };
