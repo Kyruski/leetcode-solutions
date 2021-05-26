1c8cb9ec62805c024c7e35d17c0550d0423255d8
kyruski <jamescboyett@gmail.com>
2021-05-26 08:22:41 -0700

    Completes 1669 for javascript

--- javascript/1669.merge-in-between-linked-list.js
+++ javascript/1669.merge-in-between-linked-list.js
@@ -13,5 +13,21 @@
  * @return {ListNode}
  */
 var mergeInBetween = function(list1, a, b, list2) {
-
+  let last = list2;
+  let prev = null;
+  let curr = list1;
+  let i = 0
+  while (last.next) {
+    last = last.next;
+  }
+  while (i <= b) {
+    if (i === a) prev.next = list2;
+    if (i === b) {
+      last.next = curr.next;
+      return list1
+    }
+    [prev, curr] = [curr, curr.next];
+    i++;
+  }
+  return list1;
 };
