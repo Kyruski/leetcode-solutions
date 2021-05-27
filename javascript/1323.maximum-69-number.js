021e7915589b99d968aaf8fca2d7c81a25dee345
kyruski <jamescboyett@gmail.com>
2021-05-27 08:54:44 -0700

    Completes 1323 for javascript

--- javascript/1323.maximum-69-number.js
+++ javascript/1323.maximum-69-number.js
@@ -3,4 +3,12 @@
  * @return {number}
  */
 var maximum69Number  = function(num) {
+  num = '' + num;
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] === '6') {
+      num = num.slice(0, i) + '9' + num.slice(i + 1); 
+      break;
+    }
+  }
+  return parseInt(num);
 };
