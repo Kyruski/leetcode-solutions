6d83108d2f239e4609e2deb21dd6f67711c01697
kyruski <jamescboyett@gmail.com>
2021-05-28 10:31:43 -0700

    Completes 1800 for javascript

--- javascript/1800.maximum-ascending-subarray-sum.js
+++ javascript/1800.maximum-ascending-subarray-sum.js
@@ -3,4 +3,16 @@
  * @return {number}
  */
 var maxAscendingSum = function(nums) {
+  let max = -Infinity;
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] > nums[j - 1]) sum += nums[j];
+      else break;
+    }
+    max = sum > max ? sum : max;
+    sum = 0;
+  }
+  return max;
 };
