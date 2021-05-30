dfd55e63f10ac4f2384157a660b95701e909bcce
kyruski <jamescboyett@gmail.com>
2021-05-29 19:08:13 -0700

    Completes 1446 for javascript

--- javascript/1446.consecutive-characters.js
+++ javascript/1446.consecutive-characters.js
@@ -3,5 +3,18 @@
  * @return {number}
  */
 var maxPower = function(s) {
-  
+  let countingChar = null;
+  let max = 0;
+  let currCount = 1;
+  for (let item of s) {
+    if (item === countingChar) {
+      currCount++
+    } else {
+      if (currCount > max) max = currCount;
+      currCount = 1;
+      countingChar = item;
+    }
+  }
+  max = Math.max(max, currCount);
+  return max;
 };
