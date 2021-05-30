36c9f558adda239edbedcf69a3c90f84144255fc
kyruski <jamescboyett@gmail.com>
2021-05-29 19:09:40 -0700

    Optimizes the code

--- javascript/1446.consecutive-characters.js
+++ javascript/1446.consecutive-characters.js
@@ -3,18 +3,16 @@
  * @return {number}
  */
 var maxPower = function(s) {
-  let countingChar = null;
   let max = 0;
   let currCount = 1;
-  for (let item of s) {
-    if (item === countingChar) {
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i - 1]) {
       currCount++
     } else {
       if (currCount > max) max = currCount;
       currCount = 1;
-      countingChar = item;
     }
   }
-  max = Math.max(max, currCount);
+  if (currCount > max) max = currCount;
   return max;
 };
