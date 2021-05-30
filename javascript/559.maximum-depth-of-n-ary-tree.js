4c4253240606f674256f9b4a407742bb95ba075a
kyruski <jamescboyett@gmail.com>
2021-05-30 09:53:56 -0700

    Completes 559 for javascript

--- javascript/559.maximum-depth-of-n-ary-tree.js
+++ javascript/559.maximum-depth-of-n-ary-tree.js
@@ -11,5 +11,19 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-  
+  let max = 0;
+  let currDepth = 0;
+  const traverse = node => {
+    currDepth++;
+    if (node.children !== null) {
+      for (let item of node.children) {
+        traverse(item);
+      }
+    }
+    if (currDepth > max) max = currDepth;
+    currDepth--;
+  }
+
+  if (root !== null) traverse(root);
+  return max;
 };
