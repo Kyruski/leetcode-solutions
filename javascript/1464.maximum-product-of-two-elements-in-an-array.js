c9ceb70ed064271a5ae5abdd948c79d0d3f692a5
kyruski <jamescboyett@gmail.com>
2021-06-01 08:21:15 -0700

    Completes 1464 for javascript

--- javascript/1464.maximum-product-of-two-elements-in-an-array.js
+++ javascript/1464.maximum-product-of-two-elements-in-an-array.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-  
+  let first = -Infinity;
+  let second = -Infinity;
+  for (let num of nums) {
+    if (num >= first) {
+      second = first;
+      first = num;
+    } else if (num >= second) {
+      second = num;
+    }
+  }
+  return (first - 1) * (second - 1);
 };
