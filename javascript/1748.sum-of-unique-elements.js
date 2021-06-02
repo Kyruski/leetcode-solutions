7da46f608a7520dcb35ee2cf3223b3e13e8cbdb0
kyruski <jamescboyett@gmail.com>
2021-06-02 08:29:59 -0700

    Completes 1748 for javascript

--- javascript/1748.sum-of-unique-elements.js
+++ javascript/1748.sum-of-unique-elements.js
@@ -3,4 +3,16 @@
  * @return {number}
  */
 var sumOfUnique = function(nums) {
+  const dict = {}
+  let output = 0;
+  for (let num of nums) {
+    if (dict[num] === undefined) {
+      dict[num] = true;
+      output += parseInt(num);
+    } else if (dict[num]) {
+      dict[num] = false;
+      output -= parseInt(num);
+    }
+  }
+  return output;
 };
