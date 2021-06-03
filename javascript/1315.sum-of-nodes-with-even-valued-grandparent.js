1e453e24571788eb75b71ea837272c3dcf72618d
kyruski <jamescboyett@gmail.com>
2021-06-03 08:12:43 -0700

    Completes 1315 for javascript

--- javascript/1315.sum-of-nodes-with-even-valued-grandparent.js
+++ javascript/1315.sum-of-nodes-with-even-valued-grandparent.js
@@ -11,5 +11,14 @@
  * @return {number}
  */
 var sumEvenGrandparent = function(root) {
-  
+  let total = 0;
+  const traverse = (node, p, gp) => {
+    if (!node) return;
+    if (gp) total += node.val;
+    let curr = node.val % 2 === 0;
+    traverse(node.left, curr, p);
+    traverse(node.right, curr, p);
+  }
+  traverse(root, false, false);
+  return total;
 };
