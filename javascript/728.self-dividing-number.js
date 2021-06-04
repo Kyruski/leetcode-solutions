200f0fc5e7cf19f8e9e431a0730ce987fea5a936
kyruski <jamescboyett@gmail.com>
2021-06-04 10:14:39 -0700

    Completes 728 for javascript

--- javascript/728.self-dividing-number.js
+++ javascript/728.self-dividing-number.js
@@ -4,5 +4,17 @@
  * @return {number[]}
  */
 var selfDividingNumbers = function(left, right) {
-  
+  let output = [];
+  for (let i = left; i <= right; i++) {
+    let numString = i.toString();
+    let isValid = true;
+    for (let item of numString) {
+      if (i % parseInt(item) !== 0) {
+        isValid = false;
+        break;
+      }
+    }
+    if (isValid) output.push(i);
+  }
+  return output;
 };
