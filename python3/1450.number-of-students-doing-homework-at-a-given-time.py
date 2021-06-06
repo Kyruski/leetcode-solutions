7dbe300c3c57cbe88fd6c678bc97d0df630057ae
kyruski <jamescboyett@gmail.com>
2021-06-06 14:32:14 -0700

    Completes 1450 for python3

--- python3/1450.number-of-students-doing-homework-at-a-given-time.py
+++ python3/1450.number-of-students-doing-homework-at-a-given-time.py
@@ -1,2 +1,11 @@
 class Solution:
     def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        # count = 0
+        # for i in range(len(startTime)):
+        #     if startTime[i] <= queryTime <= endTime[i]:
+        #         count += 1
+        # return count
+
+        # return sum(startTime[i] <= queryTime <= endTime[i] for i in range(len(startTime)))
+
+        return sum(s <= queryTime <= e for s, e in zip(startTime, endTime))
