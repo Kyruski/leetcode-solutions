1a8b40de5b383c6c19ae046c91e219a438df27cc
kyruski <jamescboyett@gmail.com>
2021-06-07 08:39:02 -0700

    Completes 461 for python3

--- python3/461.hamming-distance.py
+++ python3/461.hamming-distance.py
@@ -1,2 +1,5 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
+        bin_x = bin(x)[2:].zfill(31)
+        bin_y = bin(y)[2:].zfill(31)
+        return sum(b1 != b2 for b1, b2 in zip(bin_x, bin_y))
