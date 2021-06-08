21792077d13a100b474bf732e9be7ae8d0ddb145
kyruski <jamescboyett@gmail.com>
2021-06-08 10:05:50 -0700

    Completes 1880 for python3

--- python3/1880.check-if-word-equals-summation-of-two-words.py
+++ python3/1880.check-if-word-equals-summation-of-two-words.py
@@ -1,2 +1,18 @@
 class Solution:
+    # def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+
+    #     firstTotal = ''
+    #     secondTotal = ''
+    #     targetTotal = ''
+    #     for x in list(firstWord):
+    #         firstTotal += str(ord(x) - 97)
+    #     for x in list(secondWord):
+    #         secondTotal += str(ord(x) - 97)
+    #     for x in list(targetWord):
+    #         targetTotal += str(ord(x) - 97)
+    #     return int(firstTotal) + int(secondTotal) == int(targetTotal)
+    def getTotal(self, word: str) -> int:
+        return int(''.join(str(ord(i) - 97) for i in list(word)))
+
     def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+        return self.getTotal(firstWord) + self.getTotal(secondWord) == self.getTotal(targetWord)
