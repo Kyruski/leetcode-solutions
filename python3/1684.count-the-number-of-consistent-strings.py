3f3e3553befd5a951a2b57c5b60d5c0661247639
kyruski <jamescboyett@gmail.com>
2021-06-10 10:29:28 -0700

    Completes 1684 for python3

--- python3/1684.count-the-number-of-consistent-strings.py
+++ python3/1684.count-the-number-of-consistent-strings.py
@@ -1,2 +1,5 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        # return len([word for word in words if len(set(word) - set(allowed)) == 0])
+        # return len([word for word in words if re.search(f'^[{allowed}]+$', word) != None])
+        return sum(re.search(f'^[{allowed}]+$', word) != None for word in words)
