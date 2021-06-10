d8d646b8d2a3fe67c6f3a2c0bbbcb045439a1fa9
kyruski <jamescboyett@gmail.com>
2021-06-09 19:37:54 -0700

    Completes 1295 for python3

--- python3/1295.find-numbers-with-even-number-of-digits.py
+++ python3/1295.find-numbers-with-even-number-of-digits.py
@@ -1,2 +1,14 @@
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
+        # return reduce(lambda tot, num: tot + 1 if len(str(num)) % 2 == 0 else tot, nums, 0)
+
+        # total = 0
+        # for num in nums:
+        #     if len(str(num)) % 2 == 0:
+        #         total += 1
+        # return total
+
+        total = 0
+        for num in nums:
+            total += 1 if len(str(num)) % 2 == 0 else 0
+        return total
