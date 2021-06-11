12449799da8b6cc3a6d050ca3ae3dab6e272cd0c
kyruski <jamescboyett@gmail.com>
2021-06-11 16:46:55 -0700

    Completes 1773 for python3

--- python3/1773.count-items-matching-rule.py
+++ python3/1773.count-items-matching-rule.py
@@ -1,2 +1,4 @@
 class Solution:
     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        key = 0 if ruleKey == "type" else 1 if ruleKey == "color" else 2
+        return len([item for item in items if item[key] == ruleValue])
