1dc42d1c663b590473128d333db51bd63becde02
kyruski <jamescboyett@gmail.com>
2021-06-14 16:25:57 -0700

    Completes 1299 for python3

--- python3/1299.replace-elements-with-greatest-element-on-right-side.py
+++ python3/1299.replace-elements-with-greatest-element-on-right-side.py
@@ -1,2 +1,9 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
+        output = arr.copy()
+        maxNum = arr[-1]
+        for i in range(len(arr) - 2, -1, -1):
+            maxNum = max(arr[i + 1], maxNum)
+            output[i] = maxNum
+        output[-1] = -1
+        return output
