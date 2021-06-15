5f9a37c6fc9044a9188c42591b9344bfeab78e9d
kyruski <jamescboyett@gmail.com>
2021-06-15 07:43:26 -0700

    Completes 1021 for python3

--- python3/1021.remove-outermost-parentheses.py
+++ python3/1021.remove-outermost-parentheses.py
@@ -1,2 +1,17 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
+        isOuter = True
+        str = ''
+        counter = 0
+        for item in s:
+            if isOuter:
+                isOuter = False
+            elif counter == 0 and item == ')':
+                isOuter = True
+            else:
+                if item == '(':
+                    counter += 1
+                else:
+                    counter -= 1
+                str += item
+        return str
