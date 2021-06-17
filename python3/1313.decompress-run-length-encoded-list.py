88bb2a45e8d66d474b0f902367464971550a32cb
Kyruski <jamescboyett@gmail.com>
2021-06-16 17:25:55 -0700

    Completes 1313 for python3

--- python3/1313.decompress-run-length-encoded-list.py
+++ python3/1313.decompress-run-length-encoded-list.py
@@ -1,3 +1,6 @@
 class Solution:
     def decompressRLElist(self, nums: List[int]) -> List[int]:
-        
+        output = []
+        for i in range(0, len(nums), 2):
+            output.extend([nums[i + 1]] * nums[i])
+        return output
