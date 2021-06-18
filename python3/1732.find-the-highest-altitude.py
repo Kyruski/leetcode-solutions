81e25143d75147ed4ce464bdc59048b76d678be3
Kyruski <jamescboyett@gmail.com>
2021-06-17 19:29:28 -0700

    Completes 1732 for python3

--- python3/1732.find-the-highest-altitude.py
+++ python3/1732.find-the-highest-altitude.py
@@ -1,3 +1,9 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        
+        maxAlt = 0
+        curr = 0
+        for val in gain:
+            curr += val
+            if curr > maxAlt:
+                maxAlt = curr
+        return maxAlt
