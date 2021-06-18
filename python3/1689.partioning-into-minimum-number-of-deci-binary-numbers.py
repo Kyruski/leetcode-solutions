125244afa1878b5b494fd5a18e3f75293258813b
Kyruski <jamescboyett@gmail.com>
2021-06-18 10:21:01 -0700

    Completes 1689 for python3

--- python3/1689.partioning-into-minimum-number-of-deci-binary-numbers.py
+++ python3/1689.partioning-into-minimum-number-of-deci-binary-numbers.py
@@ -1,3 +1,12 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
-        
+        # maxNum = 0
+        # for num in str(n):
+        #     if int(num) > maxNum:
+        #       maxNum = int(num)
+        # return maxNum
+
+
+        # return max([int(num) for num in str(n)])
+        return max(n)
+            
