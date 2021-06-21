a17769b21c6a07b65c30a3635861a547b96dd51e
Kyruski <jamescboyett@gmail.com>
2021-06-20 17:19:56 -0700

    Completes 1630 for python3

--- python3/1630.arithmetic-subarrays.py
+++ python3/1630.arithmetic-subarrays.py
@@ -1,3 +1,14 @@
 class Solution:
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
-        
+        output = []
+        for i in range(len(l)):
+            arr = nums[l[i]: r[i] + 1]
+            arr.sort()
+            step = arr[1] - arr[0]
+            isValid = True
+            for x in range(2, len(arr)):
+                if arr[x] - arr[x - 1] != step:
+                    isValid = False
+                    break
+            output.append(isValid)
+        return output
