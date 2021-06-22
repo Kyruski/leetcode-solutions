31bd8743b781932e1964bac4275f937e2853f938
Kyruski <jamescboyett@gmail.com>
2021-06-22 13:14:04 -0700

    Completes 1365 for python3

--- python3/1365.how-many-numbers-are-smaller-than-the-current-number.py
+++ python3/1365.how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,3 +1,24 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        
+        # return [sum([y < x for y in nums]) for x in nums]
+
+        # output = []
+        # for x in nums:
+        #     count = 0
+        #     for y in nums:
+        #         if y < x:
+        #             count += 1
+        #     output.append(count)
+        # return output
+
+        dict = {}
+        output = []
+        for x in nums:
+            count = dict.get(x)
+            if dict.get(x) == None:
+                count = 0
+                for y in nums:
+                    if y < x:
+                        count += 1
+            output.append(count)
+        return output
