28ed2c746e3ec0228b5b1f5e9bac02e519624e38
Kyruski <jamescboyett@gmail.com>
2021-06-23 10:36:26 -0700

    Completes 92 for python3

--- python3/92.reverse-linked-list-ii.py
+++ python3/92.reverse-linked-list-ii.py
@@ -5,4 +5,32 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
-        
+        if left == right:
+            return head
+        currNode = head
+        prev = None
+        start = None
+        startBefore = None
+        nodeNum = 1
+        while currNode:
+            if nodeNum == left:
+                start, startBefore, currNode, prev = currNode, prev, currNode.next, currNode
+            elif nodeNum == right:
+                currNode.next, start.next = prev, currNode.next
+                if startBefore == None:
+                    head = currNode
+                else:
+                    startBefore.next = currNode
+                break 
+            elif start:
+                temp = prev
+                prev = currNode
+                currNode = currNode.next
+                prev.next = temp
+            else:
+                prev, currNode = currNode, currNode.next
+            nodeNum += 1
+        return head
+
+                
+
