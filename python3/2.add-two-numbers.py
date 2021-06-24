73b2c1ed768d4d4b65a665236290f6157851a71a
kyruski <jamescboyett@gmail.com>
2021-06-24 09:41:06 -0700

    Completes 2 for python3

--- python3/2.add-two-numbers.py
+++ python3/2.add-two-numbers.py
@@ -5,3 +5,16 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        numOne = ''
+        numTwo = ''
+        node = None
+        while l1 or l2:
+            if l1:
+                numOne = str(l1.val) + numOne
+                l1 = l1.next
+            if l2:
+                numTwo = str(l2.val) + numTwo
+                l2 = l2.next
+        for char in str(int(numOne) + int(numTwo)):
+            node = ListNode(val=int(char), next=node)
+        return node
