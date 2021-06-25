ca4415042c2fa847144b1289f4e687c16dd3025c
kyruski <jamescboyett@gmail.com>
2021-06-25 08:59:32 -0700

    Adds another solution for 1281 for python3

--- python3/1281.subtract-the-product-and-sum-of-digits-of-an-integer.py
+++ python3/1281.subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -2,9 +2,17 @@ class Solution:
     def subtractProductAndSum(self, n: int) -> int:
         # return prod([int(x) for x in str(n)]) - sum([int(x) for x in str(n)])
 
+        # sumNum = 0
+        # prodNum = 1
+        # for x in str(n):
+        #     sumNum += int(x)
+        #     prodNum *= int(x)
+        # return prodNum - sumNum
+
         sumNum = 0
         prodNum = 1
-        for x in str(n):
-            sumNum += int(x)
-            prodNum *= int(x)
+        while n > 0:
+            sumNum += n % 10
+            prodNum *= n % 10
+            n //= 10
         return prodNum - sumNum
