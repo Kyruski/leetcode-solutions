b9055af1d6c737e3d79c944146c16379e49ea494
kyruski <jamescboyett@gmail.com>
2021-06-24 22:23:35 -0700

    Completes 1 for python3

--- python3/1.two-sum.py
+++ python3/1.two-sum.py
@@ -1,2 +1,10 @@
 class Solution:
+    def __init__(self):
+        self.dict = {}
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i, num in enumerate(nums):
+            j = self.dict.get(target - num)
+            if j != None:
+                return [j, i]
+            self.dict[num] = i
