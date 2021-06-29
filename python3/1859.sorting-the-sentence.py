741b3e97b5c61d9fb067e9144f7cc60c571946eb
kyruski <jamescboyett@gmail.com>
2021-06-29 11:47:11 -0700

    Completes 1859 for python3

--- python3/1859.sorting-the-sentence.py
+++ python3/1859.sorting-the-sentence.py
@@ -1,2 +1,13 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
+        # arr1 = s.split()
+        # arr2 = [None] * len(arr1)
+        # for word in arr1:
+        #     arr2[int(word[-1]) - 1] = word[:-1]
+        # return ' '.join(arr2)
+
+        arr1 = s.split()
+        arr1.sort(key=lambda w: w[-1])
+        for i in range(len(arr1)):
+            arr1[i] = arr1[i][:-1]
+        return ' '.join(arr1)
